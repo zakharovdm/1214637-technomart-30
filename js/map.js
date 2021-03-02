5fcf4fb91b43e0d654e0e921d631c57ef33734aa
Dmitry Zakharov <dzaharov794@gmail.com>
2021-03-02 21:42:02 +0300

    Добавил интерактивность для карты

---
+++ js/map.js
@@ -0,0 +1,24 @@
+const mapPopup = document.querySelector(".modal-map");
+const mapLink = document.querySelector(".map-link");
+const mapClose = mapPopup.querySelector(".modal-close");
+
+
+mapLink.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("modal-show");
+});
+
+
+mapClose.addEventListener("click", function() {
+  mapPopup.classList.remove("modal-show");
+});
+
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    if (mapPopup.classList.contains("modal-show")){
+      evt.preventDefault();
+      mapPopup.classList.remove("modal-show");
+    }
+  }
+})
